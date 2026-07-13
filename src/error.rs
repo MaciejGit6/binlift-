0335e2a55328d38ff6aaa1684fda5c1ff4ecba2c
MaciejGit6 <macie.milewski@gmail.com>
2026-07-13 22:16:18 +0200

    feat(error): add BinliftError and crate Result alias

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,17 @@
 //! Crate-wide error type.
+
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum BinliftError {
+    #[error("unrecognized magic bytes: {0:x?}")]
+    UnknownFormat(Vec<u8>),
+
+    #[error("unexpected end of data at offset {0}")]
+    Truncated(usize),
+
+    #[error("unsupported feature: {0}")]
+    Unsupported(String),
+}
+
+pub type Result<T> = std::result::Result<T, BinliftError>;
